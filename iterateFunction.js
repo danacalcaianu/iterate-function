d13de0d31dece3a68435270229000269762f49f2
Daniela Calcaianu <daniela.calcaianu@Danielas-Mac-mini.local>
2017-10-31 17:45:49 +0200

    added more tests

--- iterateFunction.js
+++ iterateFunction.js
@@ -1,31 +1,34 @@
 const object = {
-  user: {
-      name: "somename",
-      age: 20,
-      contact: {
-          phone: "074074074",
-          address: {
-              street: "some street",
-              number: 30,
-          },
-      },
-  },
-  someFunction() {
-      console.log("here");
-  },
-  account: "someaccount",
+    user: {
+        name: "somename",
+        age: 20,
+        contact: {
+            phone: "074074074",
+            address: {
+                homeAddress: {
+                    current :{
+                        street : "some street",
+                    }
+                },
+                officeAddress: "office address",
+            },
+        },
+    },
 }
 
 function displayProps(obj, key) {
   let result = [];
+  if(Array.isArray(obj)){
+      return result;
+  }
   for ( var property in obj ) {
-      if ( typeof obj[property] == "object" ) {
+      if ( typeof obj[property] == "object" && !(Array.isArray(obj[property])) ) {
           if ( key !== undefined ) {
               result.push( ...displayProps( obj[property], key + '.' + property ) );
           } else {
               result.push( ...displayProps( obj[property], property ) );
           }
-      } else if ( typeof obj[property] !== "function" ) {
+      } else if ( typeof obj[property] !== "function") {
           if ( key !== undefined ) {
               result.push( key + '.' + property );
           } else {
--- tests/iterate.test.js
+++ tests/iterate.test.js
@@ -38,4 +38,60 @@ describe( "iterateFunction", function() {
         let result = displayProps(obj);
         expect(result).to.be.eql([]);
     } );
+    it("should ignore all array properties and functions ",function(){
+        const obj = {
+            user: {
+                name: "somename",
+                age: 20,
+                contact: {
+                    phone: "074074074",
+                    address: {
+                        street: ["some street","some city"],
+                        number: 30,
+                    },
+                },
+            },
+            someFunction() {
+                console.log("here");
+            },
+            account: ["someaccount","some other account"],
+          }
+          let result = displayProps(obj);
+          expect(result).to.be.eql([ 'user.name',
+          'user.age',
+          'user.contact.phone',
+          'user.contact.address.street',
+          'user.contact.address.number',
+          'account' ]);
+    } );
+    it("should return an empty array if the given parameter is an array", function() {
+        const obj = ["something","test"];
+        let result = displayProps(obj);
+        expect(result).to.be.eql([]);
+    })
+    it("should include also the innermost properties with the full path to them",function() {
+        const obj = {
+            user: {
+                name: "somename",
+                age: 20,
+                contact: {
+                    phone: "074074074",
+                    address: {
+                        homeAddress: {
+                            current :{
+                                street : "some street",
+                            }
+                        },
+                        officeAddress: "office address",
+                    },
+                },
+            },
+        }
+        result = displayProps(obj);
+        expect(result).to.be.eql([ 'user.name',
+        'user.age',
+        'user.contact.phone',
+        'user.contact.address.homeAddress.current.street',
+        'user.contact.address.officeAddress' ]);
+    } );
 } );
